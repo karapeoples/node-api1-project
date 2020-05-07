fa0dec5870351fb9e744203228e1c7837c062699
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-05-07 08:02:27 -0500

    Code Matches Guided Project

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ server.use(express.json())
 
 //Code To Check Running Server is Working
 server.get('/', (req, res) => {
-  res.status(200).json('Welcome to NODE API 1')
+	res.status(200).json('Welcome to NODE API 1')
 })
 
 //Project EndPoint Code (Not Including Errors as they were not covered and I am unsure how to do them without a DB 😇)
@@ -18,48 +18,67 @@ let users = []
 
 //Post (Works on PostMan)
 server.post('/api/users', (req, res) => {
-  const body = req.body
-  users.id = shortid.generate()
-  
-  users.push(body)
+	const userInfo = req.body
+	userInfo.id = shortid.generate()
 
-  !body.name || !body.bio ? res.status(400).json({ error: 'Please provide name and bio for the user' }) : res.status(201).json(body)
+	users.push(userInfo)
+
+	!userInfo.name || !userInfo.bio
+		? res.status(400).json({ error: 'Please provide name and bio for the user' })
+		: res.status(201).json(userInfo)
 })
 
 //GET (Works on PostMan)
 server.get('/api/users', (req, res) => {
-  res.status(200).json(users)
+	res.status(200).json(users)
 })
 
-//GET BY ID (Works on PostMan??Not sure how to check with ShortID) In process of review
+//GET BY ID (Cannot Get to Work on Postman)
 server.get('api/users/:id', (req, res) => {
-  const { id } = req.params
-  
-  !id ? res.status(404).json({ error: 'The USER does NOT Exist' }) :
-    res.status(200).json(req.body)
+	const { id } = req.params
 
+	const found = users.find((user) => {
+		user.id === id
+	})
+
+	if (found) {
+		res.status(200).json(found)
+	} else {
+		res.status(404).json({ error: 'The USER does NOT Exist' })
+	}
 })
 
-//DELETE (Works on PostMan?? Not sure how to check with ShortID) In process of review
-server.delete('/api/user/:id', (req, res) => {
-  const { id } = req.params
-  
-  !id ? res.status(404).json({ error: 'The USER does NOT Exist' }) : res.status(200).json(req.body)
+//DELETE (Cannot get to Work on Postman)
+server.delete('/api/users/:id', (req, res) => {
+	const { id } = req.params
 
+	const found = users.find((user) => {
+		user.id === id
+  })
+  
+	if (found) {
+		users = users.filter((user) => user.id !== id)
+		res.status(200).json(found)
+	} else {
+		res.status(404).json({ error: 'The USER does NOT Exist' })
+	}
 })
 
-
-//PUT(Works on PostMan?? Not sure how to check with ShortID) In process of review
+//PUT(Cannot Get to Work on Postman)
 server.put('/api/user/:id', (req, res) => {
-  const { id } = req.params
-  const body = req.body
+	const { id } = req.params
+	const body = req.body
 
-  !id ? res.status(404).json({ error: 'The USER does NOT Exist' }) :
-    !body.name || !body.bio ? res.status(400).json({ error: "Provide Name and Bio0" }) :
-      res.status(200).json(body)
+  let index = users.findIndex(user=> user.id ===id)
+  if (index !== -1) {
+    users[index] = body 
+    res.status(200).json(users[index])
+  }
+  else {
+    res.status(404).json({error: 'The USER does Not Exist'})
+  }
+		
 })
-
-
 
 //Server Code
 const PORT = 4994
